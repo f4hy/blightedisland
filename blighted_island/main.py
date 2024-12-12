e56e3b61bdef21a275abc0733a52409b39eb0247
Brendan Fahy <bmfahy@gmail.com>
2024-12-12 01:41:01 -0500

    randomize

--- blighted_island/main.py
+++ blighted_island/main.py
@@ -1,12 +1,32 @@
 import streamlit as st
 import spirits
+import random
+import time
 
 
 def main():
     st.title("Welcome to Blighted Island")
     st.subheader("Stats for spirit island")
 
-    st.write(spirits.SPIRITS)
+    random_spirit, game_tracker = st.tabs(
+        ["Choose a random Spirit", "Track Game Stats"]
+    )
+
+    with random_spirit:
+        choices = list(spirits.SPIRITS.keys())
+        if st.button("Randomize"):
+            selected = random.choice(choices)
+            bar = st.progress(0, text="")
+            for i in range(50):
+                time.sleep(0.1)
+                bar.progress(i * 2, text=random.choice(choices))
+            time.sleep(0.1)
+            bar.empty()
+            st.write(selected)
+            st.write(f"Complexity: {spirits.SPIRITS[selected]}")
+
+    with game_tracker:
+        st.write("soon.")
 
 
 if __name__ == "__main__":
